6bddf4bbb3654f1f0270219b9b17541f163da5c8
Jorge Neto <jorge.c.neto@hotmail.com>
2023-02-21 18:18:14 -0300

    habilitar e desabilitar os botoes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,36 @@
+import React from "react"
 import Jogo from "./Jogo"
 import Letras from "./Letras"
+import palavras from "../palavras"
+import forca0 from "../assets/forca0.png"
+import forca1 from "../assets/forca1.png"
+import forca2 from "../assets/forca2.png"
+import forca3 from "../assets/forca3.png"
+import forca4 from "../assets/forca4.png"
+import forca5 from "../assets/forca5.png"
+import forca6 from "../assets/forca6.png"
+
+
 
 function App() {
+  const[corDesabilitado, setCorDesabilitado] = React.useState("desabilitado")
+  const[desabilitado, setDesabilitado] = React.useState(true)
+  const letra = l.toUpperCase();
+
+  function start(){
+    setDesabilitado(false)  
+    setCorDesabilitado("letra")
+  }
+
+  function verificarLetra (){
+    // if(palavraRenderizada)
+    console.log(l)
+  }
+
   return (
     <div class="conteudo">
-      <Jogo />
-      <Letras />
+      <Jogo imagem={forca0} start={start}/>
+      <Letras cor={corDesabilitado} habilitar={desabilitado} verificarLetra={verificarLetra}/>
     </div>
   );
 }
--- src/components/Letras.js
+++ src/components/Letras.js
@@ -1,9 +1,9 @@
 const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-export default function Letras () {
+export default function Letras (props) {
     return(
         <div class="letrasRenderizadas">
-            {alfabeto.map((l) => <button class="letra">{l.toUpperCase()}</button>)}            
+            {alfabeto.map((l) => <button onClick={props.verificarLetra} class={props.cor} disabled={props.habilitar}>{l.toUpperCase()}</button>)}            
         </div>
     )
 }
