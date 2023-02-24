f1cb8dc544cf43ecd13d126d49d4a4b2cee811f4
Jorge Neto <jorge.c.neto@hotmail.com>
2023-02-24 11:27:46 -0300

    adição de index

--- src/components/App.js
+++ src/components/App.js
@@ -13,55 +13,73 @@ import forca6 from "../assets/forca6.png"
 
 
 function App() {
-  const[corDesabilitado, setCorDesabilitado] = React.useState("desabilitado")
   const[desabilitado, setDesabilitado] = React.useState(true)
   const[erros, setErros] = React.useState(0)
   const[imagem, setImagem] = React.useState(forca0)
   const[palavraRenderizada, setPalavraRenderizada] = React.useState("")
+  const[letrasTestadas, setLetrasTestadas] = React.useState("")
+  let palavraEscolhida;
   
 
   function start(){
     const randomIndex = Math.floor(Math.random() * palavras.length)
-    const palavraEscolhida = palavras[randomIndex]
+    palavraEscolhida = palavras[randomIndex]
     console.log(palavraEscolhida, " palavra sortida")
     setPalavraRenderizada(palavraEscolhida.replace(/[a-z\s]/g, "_ "))    
-    setDesabilitado(false)  
-    setCorDesabilitado("letra")    
+    setDesabilitado(false)     
+    setErros(0)
+    setImagem(forca0)
   }
 
-  function verificarLetra (l){
-    console.log("funcionou")
-    console.log(l)
-  //   if(palavraEscolhida.includes(" ") ){
-  //     const indexDaLetra = palavraEscolhida.findIndex(" ")
-  //   }else {
-  //     setErros(erros + 1)
- // __reactProps$e8ijusj9gel.children
-  //     if(image === forca0){
-  //       setImagem(forca1)
-  //     } else if (imagem === forca1){
-  //       setImagem(forca2)
-  //     } else if (imagem === forca2){
-  //       setImagem(forca3)
-  //     } else if (imagem === forca3){
-  //       setImagem(forca4)
-  //     } else if (imagem === forca4){
-  //       setImagem(forca5)
-  //     } else if (imagem === forca5){
-  //       setImagem(forca6)
-  //       fimDeJogo();
-  //     }
+  // function fimDeJogo (){
+
+  // }
+
+
+  // console.log(letrasTestadas)
+
+  function verificarLetra (l, index){
+    // const verificado = letrasTestadas.includes(l.innerHTML)   
+    // if (!verificado){
+    //   setLetrasTestadas([...letrasTestadas, l.innerHTML])
+    // }
+    // console.log(index, "index da letra no map")
+    
+    console.log(l.innerHTML.toLowerCase())
+    if(palavraEscolhida.includes(l.innerHTML.toLowerCase())){
+      console.log("entrou na condicional")  
+      // const indexDaLetra = palavraEscolhida.findIndex(l.innerHTML.toLowerCase())
+      // console.log(indexDaLetra, "index da letra na palavra")
+
+    }else {
+      console.log("pulou a condicional")
+      setErros(erros + 1)
+      console.log(erros, "erros")
+ 
+      if(imagem === forca0){
+        setImagem(forca1)
+      } else if (imagem === forca1){
+        setImagem(forca2)
+      } else if (imagem === forca2){
+        setImagem(forca3)
+      } else if (imagem === forca3){
+        setImagem(forca4)
+      } else if (imagem === forca4){
+        setImagem(forca5)
+      } else if (imagem === forca5){
+        setImagem(forca6)
+        // fimDeJogo();
+      }
       
-  //   }
-    // setCorDesabilitado("desabilitado")
-    // setDesabilitado(true)
+    }
+    // l.setDesabilitado(true)
     
   }
 
   return (
     <div class="conteudo">
       <Jogo imagem={imagem} start={start} palavraRenderizada={palavraRenderizada}/>
-      <Letras cor={corDesabilitado} habilitar={desabilitado} verificarLetra={verificarLetra}/>
+      <Letras cor={(desabilitado) ? "desabilitado" : "letra"} habilitar={desabilitado} verificarLetra={verificarLetra}/>
     </div>
   );
 }
--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -1,5 +1,3 @@
-
-
 export default function Jogo(props){
     return (
         <div class="jogo">
